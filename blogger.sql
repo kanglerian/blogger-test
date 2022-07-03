083a045a6505fe572e1154e2aa8233cf9728dc67
kanglerian <kanglerian@gmail.com>
2022-07-03 20:32:22 +0700

    new database

--- blogger.sql
+++ blogger.sql
@@ -1,17 +1,107 @@
-CREATE TABLE `pengguna` (
-  `nik` int PRIMARY KEY,
-  `username` varchar(50),
-  `password` varchar(20),
-  `status` boolean
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jul 03, 2022 at 03:31 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.0.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `blogger`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `artikel`
+--
 
 CREATE TABLE `artikel` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `id_pengguna` int,
-  `judul` varchar(255),
-  `sub_judul` text,
-  `konten` text,
-  `tanggal` date
-);
+  `id` int(11) NOT NULL,
+  `id_pengguna` int(11) DEFAULT NULL,
+  `judul` varchar(255) DEFAULT NULL,
+  `sub_judul` text DEFAULT NULL,
+  `konten` text DEFAULT NULL,
+  `tanggal` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `artikel` ADD FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`nik`);
+--
+-- Dumping data for table `artikel`
+--
+
+INSERT INTO `artikel` (`id`, `id_pengguna`, `judul`, `sub_judul`, `konten`, `tanggal`) VALUES
+(10, 201702102, 'dasdas', 'dasdas', 'dasdas', '2022-07-05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengguna`
+--
+
+CREATE TABLE `pengguna` (
+  `nik` int(11) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pengguna`
+--
+
+INSERT INTO `pengguna` (`nik`, `username`, `password`, `status`) VALUES
+(201702102, 'kanglerian', 'kanglerian', 0),
+(201702103, 'sopyan', 'sopyan', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `artikel`
+--
+ALTER TABLE `artikel`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_pengguna` (`id_pengguna`);
+
+--
+-- Indexes for table `pengguna`
+--
+ALTER TABLE `pengguna`
+  ADD PRIMARY KEY (`nik`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `artikel`
+--
+ALTER TABLE `artikel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `artikel`
+--
+ALTER TABLE `artikel`
+  ADD CONSTRAINT `artikel_ibfk_1` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`nik`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
